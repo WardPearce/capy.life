275a77366280266ea67a5e2f6a984e85f5f08136
WardPearce <wardpearce@protonmail.com>
2022-04-06 13:08:35 +1200

    Started svelte frontend

---
+++ frontbara/src/App.svelte
@@ -0,0 +1,18 @@
+<main>
+    <section>
+        <h1>Capybara of the day!</h1>
+        <img src="./src/assets/img/capy.jpg" alt="capy">
+
+        <h3>What is capy.life?</h3>
+        <p>Capy.Life is the official daily Capybara site approved by many Zoos & Scientists. 1/10 Endocrinologist say that viewing a Capybara each day drastically improves your mental state.</p>
+
+        <h3>Submit a capybara!</h3>
+        <p>woo</p>
+
+        <h3>Timeline</h3>
+        <p>View the history of capybaras</p>
+    </section>
+    <footer>
+        <p>Everything on this website is satire & is NOT factually accurate.</p>
+    </footer>
+</main>
---
+++ frontbara/src/main.ts
@@ -0,0 +1,9 @@
+import App from './App.svelte'
+
+import './assets/css/style.css'
+
+const app = new App({
+  target: document.getElementById('app')
+})
+
+export default app
