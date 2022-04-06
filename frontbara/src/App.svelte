d55901f9a31192993e057e880c717667e4aaeda2
WardPearce <wardpearce@protonmail.com>
2022-04-06 14:02:56 +1200

    Minor style changes

--- frontbara/src/App.svelte
+++ frontbara/src/App.svelte
@@ -1,16 +1,29 @@
+<script lang="ts">
+    let page = 0
+</script>
+
 <main>
     <section>
         <h1>Capybara of the day!</h1>
         <img src="./src/assets/img/capy.jpg" alt="capy">
 
-        <h3>What is capy.life?</h3>
+        <h2>What is capy.life?</h2>
         <p>Capy.Life is the official daily Capybara site approved by many Zoos & Scientists. 1/10 Endocrinologist say that viewing a Capybara each day drastically improves your mental state.</p>
 
-        <h3>Submit a capybara!</h3>
-        <p>woo</p>
+        <h2 id="submit">Submit a capybara!</h2>
+        <form>
+            <label for="capy-name">Name <span style="font-size: .7em;">(optional)</span></label>
+            <input type="text" name="capy-name" placeholder="e.g. greg">
+            <label for="capy-file">Image</label>
+            <input required type="file" name="capy-file" accept="image/png,image/gif,image/jpeg,image/jpg">
+            <label for="capy-captcha">Captcha</label>
+            <input type="text" name="capy-captcha" placeholder="capy">
+            <button type="submit">Submit</button>
+        </form>
 
-        <h3>Timeline</h3>
-        <p>View the history of capybaras</p>
+        <h2>Timeline</h2>
+        <p style="padding-bottom: 1em;">View the history of capybaras!</p>
+        <button on:click={() => page++}>Load more</button>
     </section>
     <footer>
         <p>Everything on this website is satire & is NOT factually accurate.</p>
