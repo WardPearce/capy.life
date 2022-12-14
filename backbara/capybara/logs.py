90c7841eda9da1ea6e36b03871d4cd836bb32be9
WardPearce <wardpearce@protonmail.com>
2022-12-14 23:07:55 +1300

    deleted old

--- backbara/capybara/logs.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-from sys import stdout
-
-LOGGER = logging.getLogger("capylife")
-LOGGER.setLevel(logging.DEBUG)
-LOGGER.addHandler(logging.StreamHandler(stdout))
--- frontbara/src/stores.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte-local-storage-store'
-
-export const adminStore = writable('admin', {
-    is: false,
-    root: false
-})
